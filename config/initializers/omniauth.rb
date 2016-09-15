e1fa2219f2cb97853e47604cf82b9c5331b91425
CJ Jessett <email": null>
2016-09-15 15:39:47 -0500

    spotify config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, 'app_id', 'app_secret', scope: 'user-read-email
-                                                     playlist-read-private
-                                                     playlist-modify-public
-                                                     playlist-modify-private
-                                                     playlist-read-collaborative'
+  provider :spotify, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], scope: 'user-read-email
+                                                                     playlist-read-private
+                                                                     playlist-modify-public
+                                                                     playlist-modify-private
+                                                                     playlist-read-collaborative'
 end
