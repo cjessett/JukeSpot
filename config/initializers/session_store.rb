4170962a1d9464631ce2bd4bfd09cfcb1c2cb7d2
CJ Jessett <cjessett@gmail.com>
2016-09-20 19:44:14 -0500

    edit domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_juke-spot_session', domain: 'https://juke-spot.herokuapp.com/'
+Rails.application.config.session_store :cookie_store, key: '_juke-spot_session', domain: 'juke-spot.herokuapp.com'
