264201b56848fecf52c73c522a00b5deb752aafb
CJ Jessett <cjessett@gmail.com>
2016-09-20 19:39:54 -0500

    change domain to heroku in session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_juke-spot_session', domain: 'localhost'
+Rails.application.config.session_store :cookie_store, key: '_juke-spot_session', domain: 'https://juke-spot.herokuapp.com/'
